b376dcd2804fc69f5f276c0dea4f1396dfad6d45
rojomaldit <lucaserfurt@hotmail.com>
2016-06-01 10:52:22 -0300

    Anda todo lo de grabar y reproducir audios

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,61 @@
-angular.module('everything.controllers', [])
+angular.module('app.controllers', [])
 
-  .controller('everythingCtrl', function($scope) {
-    $scope.tasks = [
-      { Acc: 'Accelerometer' },
-      { Aud: 'alertGrabar' },
-      { Not: 'verNotificacion' },
-      { Viv: 'Vibrar' }
-    ];
+.controller('reproducirCtrl', function($scope) {
+	
+})
+   
+.controller('grabarAudioCtrl', function($scope) {
+	var file;
+	var path;
+	var media;
+	
+	function falloGrabacion(){
+		alert("fallo grabación");
+	}
 
+	function exitoGrabacion(media){
+		media.release();
+	}
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-    function alertDismissed()
-    {
-    // hacer algo
-    }
-    $scope.verNotificacion = function()
-    {
-      navigator.notification.alert('Seguimos Avanzando!',alertDismissed,'Soy una Notificación','Aceptar');
-    }
-///////////////////////////////////////////////////////////////////////////////////////////////
-    var file;
-    var path;
-    function capturarAudio(mediaFiles){
-      var i, len;
-      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-        path = mediaFiles[i].fullPath;
-        // do something interesting with the file
-        file = mediaFiles[0].localURL;
-      } 
-    }
-    function captureError(){
-    alert("Cancelo la grabación");
-    }
+	function capturarExito(mediaFiles){
+		var i, len;
+	    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+	        path = mediaFiles[i].fullPath;
+	        // do something interesting with the file
+	        file = mediaFiles[0].localURL;
+	    } 
+	    media = new Media(file, exitoGrabacion, falloGrabacion);
+	}
 
-    $scope.alertGrabar = function(){
-    navigator.device.capture.captureAudio(capturarAudio, captureError); 
-    }
-  });
-///////////////////////////////////////////////////////////////////////////////////////////////
+	function captureError(){
+		alert("La grabacion a fallado");
+	}
 
+	$scope.capturarAudio = function (mediaFiles){
+		navigator.device.capture.captureAudio(capturarExito, captureError); 
+	}
 
+	$scope.reproducirAudioGrabado = function(){
+		//var media = new Media(file, exitoGrabacion, falloGrabacion);
+
+		if (typeof file === "undefined") {
+	    	navigator.notification.alert("Primero debe grabar un Audio.", null, "Mensaje en Reproducir", "Aceptar");
+			return; 
+	    }
+	    media.play();
+		}
+
+		$scope.pausarAudio = function(){
+			media.pause();
+		}
+
+		$scope.pararAudio = function(){
+			media.stop();
+		}
+	})
+   
+.controller('grabacionesCtrl', function($scope) {
+
+   
+})
+       
